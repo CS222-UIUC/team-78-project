1a580dd4d2d37465e1710c853bed7c3bd36b5644
Jeslin Paulraj <jeslin.paulraj@gmail.com>
2025-03-30 14:56:46 -0500

    created default pages for website and added homepage, account settings, logo, and footer features to each page

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html') 
 
+@app.route('/favorites.html')
+def favorites():
+    return render_template('favorites.html')
+
+@app.route('/models.html')
+def models():
+    return render_template('models.html')
+
+@app.route('/stock_analysis.html')
+def stock_analysis():
+    return render_template('stock_analysis.html')
+
+@app.route('/stock_comparison.html')
+def stock_comparison():
+    return render_template('stock_comparison.html')
+
+@app.route('/account_settings.html')
+def account_settings():
+    return render_template('account_settings.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
