21e390a5be9397d1dc4c075e05b93a6935a6f8eb
Jeslin Paulraj <jeslin.paulraj@gmail.com>
2025-05-14 21:10:22 -0400

    resolving merge issues

--- database_setup.py
+++ database_setup.py
@@ -1,35 +1,40 @@
 import sqlite3
-import bcrypt  
+from werkzeug.security import generate_password_hash  
 
 connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
-
-# cursor.execute("DROP TABLE IF EXISTS users") #done to restructure to add emails to db
 
 # users table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        email TEXT UNIQUE NOT NULL
+        password TEXT NOT NULL
     )
 ''')
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS favorites (
+        id       INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id  INTEGER NOT NULL,
+        symbol   TEXT NOT NULL,
+        UNIQUE(user_id, symbol),          -- prevents duplicates
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    )
+''')
+connection.commit()
 
-def add_user(username, plaintext_password, email):
-    hashed_password = bcrypt.hashpw(plaintext_password.encode('utf-8'), bcrypt.gensalt())
-    
+def add_user(username, plaintext_password):
+    hashed_password = generate_password_hash(plaintext_password)
     try:
-        cursor.execute('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', 
-                       (username, hashed_password.decode('utf-8'), email))
-        
+        cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', 
+                       (username, hashed_password.encode('utf-8')))
         connection.commit()
-        print(f"User '{username}' with email '{email}' added successfully!")
+        print(f"User '{username}' added successfully!")
     except sqlite3.IntegrityError:
-        print(f"User '{username}' or email '{email}' already exists!")
+        print(f"User '{username}' already exists!")
 
-rows = cursor.execute("SELECT * FROM users").fetchall()
+add_user('testuser', 'password123') # for debugging tried making sample user
 
 connection.close()
 print("All Done! db and user setup is success!")
